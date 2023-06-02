cd2f6457e8f0254a1ce75a77a347565fd82ba2e0
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-02 23:50:43 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
-  return (
-    <div>
-      Login page
-    </div>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your form submission logic here
+  };
+
+  return (
+    <div className="login-form-container">
+
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

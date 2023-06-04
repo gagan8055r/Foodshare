ef5779dfd86a785fbe23e47310181c5d7290b064
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-04 17:15:40 +0530

    Update Mission.js

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from 'react';
+import './Mission.css'; 
 const Mission = () => {
   return (
-    <div>
-      mission page
-    </div>
-  )
+    <div className="mission-grid">
+    
+       
+      <div className="displaygrid" >
+ </div>
+        <div className="content">
+
+          <p>Our Mission: 
+Combating Hunger, Reducing Food Waste, and Nourishing Communities
+
+At <b>FOOD CONNECT</b>, our mission is to create a world where no one goes to bed hungry. We are dedicated to combating food insecurity, reducing food waste, and nourishing communities in need.
+
+Through our platform, we aim to connect food surplus with those facing hunger, ensuring that edible food doesn't go to waste. We work closely with local food businesses, farmers, and volunteers to collect and redistribute surplus food to organizations and individuals who lack access to nutritious meals.<br></br>
+<br></br>
+Key elements of our mission include:
+<br></br>
+<br></br>
+
+1. Food Recovery: We collaborate with food establishments, including restaurants, grocery stores, and farms, to rescue excess food that would otherwise be discarded. By diverting this food from the waste stream, we can make a significant impact in reducing food waste. <br></br>
+<br />
+
+2. Redistribution: We efficiently distribute the collected surplus food to community organizations, food banks, shelters, and other initiatives that support individuals
+</p>
+        </div>
+     
+      </div>
+  
+  );
 }
 
-export default Mission
+export default Mission;

1e2c6894b6b4f24e61ed229e5e3f622939e3176e
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-05 22:42:35 +0530

    Create Donor.js

---
+++ src/components/Donor.js
@@ -0,0 +1,87 @@
+
+import React, { useState } from 'react';
+import './Donor.css';
+import { useNavigate } from 'react-router-dom';
+
+
+const Donor = () => {
+
+  const navigate = useNavigate();
+  const [userData, setUserdata] = useState({
+    email:"",
+    password:"",
+  });
+
+  let name,value;
+  const postChange=(e)=>{
+name=e.target.name;
+value=e.target.value;
+
+setUserdata({...userData,[name]:value})
+
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const {email,password}=userData;
+    const res=await fetch('https://foodconnect-a8083-default-rtdb.firebaseio.com/userDataRecord/donorRecord.json',
+      {  method:'POST',
+      headers:{
+      'Content-Type':'application/json',
+  },
+  body:JSON.stringify({
+    email,
+    password,
+  }),
+    }
+    );
+ if(res)
+ {
+  setUserdata({
+    email:"",
+    password:""
+  })
+  
+ 
+  navigate('/donor/details');
+ }
+ else
+ {
+  alert("Please fill the data")
+ }
+  };
+
+  return (
+    <div className="login-form-container">
+    
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <h2>DONOR'S LOGIN</h2>
+          <label htmlFor="username">Email:</label>
+          <input
+          name='email'
+            type="email"
+            id="email"
+            value={userData.email}
+            onChange={postChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+          name='password'
+            type="password"
+            id="password"
+            value={userData.password}
+            onChange={postChange}
+            required
+          />
+        </div>
+        <button type="submit" >Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Donor;

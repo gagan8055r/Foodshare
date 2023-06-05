62b7cfce2879c1a196356527259a869248e79fd5
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-05 22:40:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
+import Donor from './components/Donor';
 import Mission from './components/Mission';
 import "./App.css"
 import Login from './components/Login'
 import Contact from './components/Contact';
-
+import Details from './components/Details';
 function App() {
 
   return (
     <>
- <div>
-
- </div>
 <Routes>
 
       <Route exact path='/' element={<Home></Home>}></Route>
-      <Route  exact path='/about' element={<About></About>} ></Route>
+      <Route  exact path='/donor' element={<Donor></Donor>} ></Route>
+      <Route  exact path='/donor/details' element={<Details></Details>} ></Route>
       <Route path='/mission' element={<Mission></Mission>}></Route>
       <Route path='/login' element={<Login></Login>}></Route>
       <Route path='/contact' element={<Contact></Contact>}></Route>
 
 </Routes>
+
       
 
 </>

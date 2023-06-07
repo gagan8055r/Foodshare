4b334aba9f73fae31adc02393970699b823a63a6
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-07 23:24:33 +0530

    Create AgentDetails.js

---
+++ src/components/AgentDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./AgentDetails.css";
+const AgentDetails = () => {
+  return (
+    <div>
+      <h2>Agent details</h2>
+    </div>
+  )
+}
+
+export default AgentDetails

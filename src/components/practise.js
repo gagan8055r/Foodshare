3578e6602215be8ae62ba441f3d85effb00c1db7
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-10 12:41:32 +0530

    Update practise.js

--- src/components/practise.js
+++ src/components/practise.js
@@ -17,81 +17,134 @@
 //     myInfo:{
 //         realName:"gags",
 //         fontName:"big",
-//     },
-//     myAge:21,
-//     getData(){
-// console.log(`my realname is ${this.myInfo.realName} and fontname is ${this.myInfo.fontName} and my age is ${this.myAge}`);
-//     }
+// //     },
+// //     myAge:21,
+// //     getData(){
+// // console.log(`my realname is ${this.myInfo.realName} and fontname is ${this.myInfo.fontName} and my age is ${this.myAge}`);
+// //     }
+// // }
+// // // bioData.getData();
+
+// const array=["gag","lal","gav"]
+
+// // console.log(array[0]);
+// // console.log(array[1]);
+// // console.log(array[2]);
+// // console.log(...array,);
+
+// // const[first,second,third]=array
+// // console.log(first);
+// // console.log(second);
+// // console.log(third);
+// // let name="vinod"
+// // // let sum=20+6
+// // const obj={
+// //     [name]:"hello",
+// //     age:21,
+// //     branch:"ece",
+// //     [15+5]:"is my age"
+// // }
+// // // let age=obj.age
+// // // let{name,age,branch}=obj
+// // // console.log(branch);
+// // // console.log(name);
+// // // console.log(age);
+// // // console.log(Object.values(obj));
+// // console.log(Object.entries(obj));
+
+// // let place="'bengaluru'"
+// // console.log(place.padStart(15));
+// // console.log(place.padEnd(15));
+
+// ////if i have array inside array inside and inside array in a array to faltten this  we use flat function console.log(array.map(3))
+
+
+// // const obj={
+// //     age:21,
+// //     place:'mysore'
+// // };
+
+// // console.log(obj);
+
+// // const fat=Object.entries(obj)
+// // console.log(Object.fromEntries(fat));
+// // // const arrays=fat.flat(1)
+// // console.log(arrays[3]);
+// // const num=Number.MAX_SAFE_INTEGER
+// // const big=console.log(9007199254740991+211);
+// // console.log(typeof big);
+
+// // const full=NaN??"default"
+
+
+// function add (a,b)
+// {
+//     return a+b;
 // }
-// // bioData.getData();
 
-const array=["gag","lal","gav"]
-
-// console.log(array[0]);
-// console.log(array[1]);
-// console.log(array[2]);
-// console.log(...array,);
-
-// const[first,second,third]=array
-// console.log(first);
-// console.log(second);
-// console.log(third);
-// let name="vinod"
-// // let sum=20+6
-// const obj={
-//     [name]:"hello",
-//     age:21,
-//     branch:"ece",
-//     [15+5]:"is my age"
+// function sub(a,b)
+// {
+//     return a-b;
 // }
-// // let age=obj.age
-// // let{name,age,branch}=obj
-// // console.log(branch);
-// // console.log(name);
-// // console.log(age);
-// // console.log(Object.values(obj));
-// console.log(Object.entries(obj));
 
-// let place="'bengaluru'"
-// console.log(place.padStart(15));
-// console.log(place.padEnd(15));
+// const calculator=(num1,num2,oper)=>{
+// return oper(num1,num2);
 
-////if i have array inside array inside and inside array in a array to faltten this  we use flat function console.log(array.map(3))
+// }
 
+// console.log(calculator(2,3,add));
+// console.log(calculator(2,3,sub));
+// // console.log(full);
 
-// const obj={
-//     age:21,
-//     place:'mysore'
-// };
+// var myName;
+// let myName="dcvfbn";
+// let myName="dfghj"
+// var myName="dfghj"
+// var myName="dfghj"
+// console.log(myName);
+// // myName="gagan"
+// console.log(myName);
 
-// console.log(obj);
+// hoisting is the property in js where the variable name defined using var keyword 
 
-// const fat=Object.entries(obj)
-// console.log(Object.fromEntries(fat));
-// // const arrays=fat.flat(1)
-// console.log(arrays[3]);
-// const num=Number.MAX_SAFE_INTEGER
-// const big=console.log(9007199254740991+211);
-// console.log(typeof big);
+// const fun1=()=>{
 
-// const full=NaN??"default"
+//     console.log("this is fun 1");
+//   fun2();
+//   console.log("this is fun 1 again");
+// }
 
+// const fun2=()=>{
+//     console.log("this is fun 2");
+// }
 
-function add (a,b)
-{
-    return a+b;
+// fun1();
+// const fun1=()=>{
+
+//     console.log("this is fun 1");
+//   fun2();
+//   console.log("this is fun 1 again");
+// }
+
+// const fun2=()=>{
+
+//     setTimeout(()=>{
+
+//         console.log("this is fun 2");
+//     },2000)
+// }
+
+// fun1();
+
+function sum(num1){
+return function(num2){
+    return function(num3)
+    {
+        return num1+num2+num3;
+    }
+}
 }
 
-function sub(a,b)
-{
-    return a-b;
-}
 
-const calculator=(num1,num2,oper)=>{
-return oper(num1,num2);
+console.log(sum(5)(3)(5));
 
-}
-
-console.log(calculator(2,3,add));
-console.log(calculator(2,3,sub));
-// console.log(full);

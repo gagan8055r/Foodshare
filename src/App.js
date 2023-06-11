9654e0a1fa0e47c728200ae552d3dabe56f55063
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-11 21:01:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css"
 import Login from './components/Login'
 import Contact from './components/Contact';
 import Details from './components/Details';
+import AgentDetails from './components/AgentDetails';
 function App() {
 
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route exact path='/' element={<Home></Home>}></Route>
       <Route  exact path='/donor' element={<Donor></Donor>} ></Route>
       <Route  exact path='/donor/details' element={<Details></Details>} ></Route>
+      <Route  exact path='/donor/agentdetails' element={<AgentDetails></AgentDetails>} ></Route>
       <Route path='/mission' element={<Mission></Mission>}></Route>
       <Route path='/login' element={<Login></Login>}></Route>
       <Route path='/contact' element={<Contact></Contact>}></Route>

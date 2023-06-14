1c0f17f792e6303b215d72a11930150566352c61
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-14 16:57:37 +0530

    Create thanks.js

---
+++ src/components/thanks.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Thanks.css'; 
+const Thanks = () => {
+  return (
+ 
+    
+  
+       <div className="contents">
+
+    <p>Dear Food Donor,  <br></br>
+<br />
+On behalf of <b>FOOD CONNECT</b>, we extend our heartfelt gratitude for your invaluable support in combating hunger and nourishing lives. Your dedication and generosity have made a profound impact on our mission to ensure that no one goes to bed hungry.
+
+Your contributions of food have not only filled empty stomachs but have also brought smiles to countless faces. Your compassion and willingness to lend a helping hand have inspired hope and renewed faith in humanity.
+
+Through your kindness, you have touched the lives of individuals and families who were struggling to put food on the table. Your donation has provided them with sustenance and relief during challenging times.
+
+We deeply appreciate your commitment to making a positive difference in our community. Your continued support is instrumental in our ongoing efforts to fight hunger and build a brighter future for those in need.
+
+Thank you for being a beacon of hope and for joining us on this important journey of compassion. Together, we can create a world where every person has access to nutritious food and the opportunity for a better life.
+<hr />
+{/* <br /> */}
+<p style={{position:"relative",left:"800px"}}>With profound gratitude,</p>  
+<b style={{position:"relative",left:"800px"}}>FOOD CONNECT</b>
+
+
+
+
+
+</p>
+       </div>
+
+
+ 
+  )
+}
+
+export default Thanks

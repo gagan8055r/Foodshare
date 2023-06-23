2ce5f114d2584ac95faf644d9ac4ea3437b7f463
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-23 23:00:28 +0530

    Update Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,11 +4,13 @@ import { collection,doc,getDocs } from 'firebase/firestore'
 import db from '../components/firebase'
 import { useNavigate } from 'react-router-dom';
 // import Address from './Address';
-import AgentDetails from './AgentDetails';
+// import AgentDetails from './AgentDetails';
 const Details = () => {
 const navigate = useNavigate();
     const[products,setProducts]=useState({})
-    const[address,setAddress]=useState('')
+    const[address,setAddress]=useState({
+      name:""
+    })
 
     const location=address
  useEffect(() => {
@@ -26,8 +28,7 @@ const navigate = useNavigate();
     
     fetchData();
   }, []);
-  <AgentDetails data={location}></AgentDetails>
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await fetch('https://foodconnect-a8083-default-rtdb.firebaseio.com/userDataRecord/addressRecord.json', {
       method: 'POST',
@@ -36,31 +37,32 @@ const handleSubmit = async (e) => {
       },
       body: JSON.stringify({
         address,
-       
+        
       }),
     });
-
+    
     if (res.ok) {
       setAddress('')
       alert("form submitted")
       console.log(address);
-
+      
       navigate('/donor/details/thanks');
-   
+      
     } else {
       alert("Please fill the address");
     }
   };
-
+  
+  // <AgentDetails data={location}></AgentDetails>
   return (
     <div>
     <div className='container'>
         <h2 style={{color:"tomato"}}> Data Fetched</h2>
         {Object.keys(products).map((id) => (
           <div key={id}>
-            <h2 style={{color:"skyblue"}}>Humidity</h2><h3>{products[id].Humidity}</h3>
-           <h2 style={{color:"skyblue"}}>Temperature</h2> <h3>{products[id].Temperature}</h3>
-           <h2 style={{color:"skyblue"}}>LightIntensity</h2> <h3>{products[id].LightIntensity}</h3>
+            <h2 >Humidity</h2><h3>{products[id].Humidity}</h3>
+           <h2 >Temperature</h2> <h3>{products[id].Temperature}</h3>
+           <h2 >LightIntensity</h2> <h3>{products[id].LightIntensity}</h3>
           </div>
         ))}
       </div>

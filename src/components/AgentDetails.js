23fe15ca31ef4d238407bbede1342883b0b65107
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-06-24 00:26:58 +0530

    Update AgentDetails.js

--- src/components/AgentDetails.js
+++ src/components/AgentDetails.js
@@ -2,12 +2,14 @@ import React,{useState,useEffect} from 'react'
 import "./AgentDetails.css";
 import { collection,doc,getDocs } from 'firebase/firestore'
 import db from '../components/firebase'
+import { useLocation } from 'react-router-dom';
 
 
 const AgentDetails = () => {
  
     const[products,setProducts]=useState({})
-
+const loc=useLocation()
+console.log(loc.state);
     
  useEffect(() => {
     const fetchData = async () => {
@@ -38,7 +40,8 @@ const AgentDetails = () => {
             <h2>Humidity</h2><h3>{products[id].Humidity}</h3>
            <h2>Temperature</h2> <h3>{products[id].Temperature}</h3>
            <h2>LightIntensity</h2> <h3>{products[id].LightIntensity}</h3>
-
+<h3>{loc.state.id}</h3>
+           {/* <h2>loanbhjvh</h2>  */}
        
           </div>
         ))}

fd141ad267856bc3c7c339f28ca1a8b82a9c1878
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-07-04 22:01:03 +0530

    Update Donor.js

--- src/components/Donor.js
+++ src/components/Donor.js
@@ -1,6 +1,5 @@
 
 // import React, { useState } from 'react';
-// import './Donor.css';
 // import app from './firebase'
 // // import { Route, Routes } from 'react-router-dom';
 // // import Details from './Details';
